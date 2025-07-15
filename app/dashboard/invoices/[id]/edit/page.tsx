4346ae5b3e5f3a3c34af6d3b61ced14ddafc21b6
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2025-07-15 18:38:45 +0900

    14章　完了

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from "next/navigation";
 
 export default async function page(props: {params: Promise<{id: string}>}) {
   const params = await props.params;
@@ -9,6 +10,10 @@ export default async function page(props: {params: Promise<{id: string}>}) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
